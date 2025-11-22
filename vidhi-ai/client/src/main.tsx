5a13e8fd3ddcc31f487059c330826bb275abbe29
Bhargav Karkare <83118429+BhargavKarkare@users.noreply.github.com>
2025-11-22 18:37:55 +0530

    Update main.tsx

--- vidhi-ai/client/src/main.tsx
+++ vidhi-ai/client/src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+// src/main.tsx  (or src/index.tsx / src/index.tsx depending on your filename)
 import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
 import App from './App'
 import './index.css'
 
 import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <ErrorBoundary>
-            <App />
-        </ErrorBoundary>
-    </React.StrictMode>,
+  <StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>,
 )
